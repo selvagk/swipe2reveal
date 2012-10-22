88b47c3a065ecbe6412c632bcf4c7221e333cf71
Selvaganapathy Kaliamurthy <selvab2v@gmail.com>
2012-10-22 13:52:58 +0530

    Updating beta , with bugs fixed

--- assests/app.js
+++ assests/app.js
@@ -12,3 +12,84 @@
 **/
 
 
+
+/***Swipe Detection****/
+$.fn.swipe = function(options) {
+		options = jQuery.extend({
+			threshold: {
+				x: 30,
+				y: 100
+			},
+			swipeLeft: function() {
+				alert('swiped left');
+			},
+			swipeRight: function() {
+				alert('swiped right');
+			}
+		}, options);
+
+ 
+
+		$(this).each(function() {
+			var me = $(this);
+			var originalCoord = {
+				x: 0,
+				y: 0
+			};
+			var finalCoord = {
+				x: 0,
+				y: 0
+			};
+
+			function touchMove(event) {
+			
+				event.preventDefault();
+				finalCoord.x = event.originalEvent.touches[0].pageX;
+				finalCoord.y = event.originalEvent.touches[0].pageY;
+			}
+
+			function touchEnd(event) {
+				var changeY = originalCoord.y - finalCoord.y;
+				if (changeY < options.threshold.y && changeY > (options.threshold.y * -1)) {
+					changeX = originalCoord.x - finalCoord.x;
+					if (changeX > options.threshold.x) {
+						options.swipeLeft.call(this);
+					}
+					if (changeX < (options.threshold.x * -1)) {
+						options.swipeRight.call(this);
+					}
+				}
+			}
+
+			function touchStart(event) {
+			
+				originalCoord.x = event.originalEvent.targetTouches[0].pageX;
+				originalCoord.y = event.originalEvent.targetTouches[0].pageY;
+				finalCoord.x = originalCoord.x;
+				finalCoord.y = originalCoord.y;
+			}
+			me.bind("touchstart MozTouchDown", touchStart);
+			me.bind("touchmove MozTouchMove", touchMove);
+			me.bind("touchend MozTouchRelease", touchEnd);
+		});
+	};
+	
+	/***function to create Swipe element***/
+	
+	
+	$(function(){
+	
+	
+	$('section').find('li').swipe({
+					swipeLeft: function() {
+					$('section').find('li').removeClass('swipedL');
+					$(this).addClass('swipedL');
+					},
+					swipeRight: function() {
+						$(this).removeClass('swipedL')
+					}
+				});
+	
+	
+	});
+
