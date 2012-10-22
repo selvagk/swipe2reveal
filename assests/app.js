0fa0da7a2fb716d94be24cb25ed5e3825a70a99f
Selvaganapathy Kaliamurthy <selvab2v@gmail.com>
2012-10-22 12:23:22 +0530

    secondary commit

---
+++ assests/app.js
@@ -0,0 +1,14 @@
+
+/*!
+	Swipe to reveal - Twitter Mobile slider v1.0
+	Original by Selvaganapathy Kaliamurthy: http://selvagk.com
+	Get the latest version: https://github.com/selvagk/swipe2Reveal
+
+ *   @name app.css
+  *   @author Selvagk
+ *   @version 0.1
+ *   @Website :www.selvagk.com
+ 
+**/
+
+
